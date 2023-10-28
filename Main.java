401b9b24c8b99d57b5486c296d07b28c7fcab661
vm_shirou <140561304+vmshirou@users.noreply.github.com>
2023-10-28 01:39:02 -0400

    actualizo
    se actualizo con un frame

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
 public class Main{
     public static void main(String args){
-      
+        JFrame es = new JFrame();
+        es.setTitle("PrimerRepositorio");
+        es.setBounds(200, 200, 400, 400);
+        es.setVisible(true);
+        es.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
